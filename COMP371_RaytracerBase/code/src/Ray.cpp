eba64128ef20d38d7dca65df074d59e858890f53
Diana <edvi.diana@gmail.com>
2026-02-05 23:01:53 -0500

    feat: implemented ray class

--- COMP371_RaytracerBase/code/src/Ray.cpp
+++ COMP371_RaytracerBase/code/src/Ray.cpp
@@ -1,2 +1,11 @@
 #include "Ray.h"
 
+#include <Eigen/Dense>
+
+Ray::Ray(const Eigen::Vector3f& origin, const Eigen::Vector3f direction) : origin(origin), direction(direction){};
+
+// P(t) = O + t*D
+// t is a scalar, where along the ray vector we are 
+Eigen::Vector3f Ray::at(double t) const {
+    return origin + t*direction;
+}
--- COMP371_RaytracerBase/code/src/Ray.h
+++ COMP371_RaytracerBase/code/src/Ray.h
@@ -3,9 +3,16 @@
 #include <Eigen/Dense>
 
 
-struct Ray {
-    Eigen::Vector3f origin;
-    Eigen::Vector3f direction;
+class Ray {
+    public:
+        Ray(){};
+        Ray(const Eigen::Vector3f& origin, const Eigen::Vector3f direction);
+        const Eigen::Vector3f getOrigin() const {return origin;}
+        const Eigen::Vector3f getDirection() const {return direction;}
+        Eigen::Vector3f at(double t) const;
+    private:
+        Eigen::Vector3f origin;
+        Eigen::Vector3f direction;
 };
 
 
